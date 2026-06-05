413d66b8b12d779a2ef2486762aa350f88a1eebc
Developer Mahad Aly <dev.mahadalyploso@gmail.com>
2026-06-06 04:58:39 +0700

    refactor file

--- src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="h-12 flex items-center justify-center border-t text-sm text-zinc-500 bg-white dark:bg-zinc-900">
-      © {new Date().getFullYear()} - Your App
-    </footer>
-  );
-}
